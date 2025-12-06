9061f198b82ac29e97501d4655dfe729729ada08
Alejandro Macia Hernandez <alejandro.macia@estudiante.uam.es>
2025-12-06 01:24:39 +0100

    Updated grammar for full mathematic correctness

--- tester.py
+++ tester.py
@@ -1,6 +1,19 @@
+import math
+from typing import List
 from python.lexer import Lexer
 from python.parser_ import Parser
 from python.interpreter import Interpreter
+
+
+def print_test_header(name: str) -> None:
+    print(f"\n[Test] {name}")
+    
+def print_test_summary(pass_count: int, test_count: int, fails: List) -> None:
+    print(f"\n{pass_count}/{test_count} tests passed.")
+    if fails:
+        print("Failed tests:")
+        for entry, expected, result in fails:
+            print(f"[X] {entry} -> {result} (Expected: {expected})")
 
 
 with open('tests.txt', 'r') as file:
@@ -12,14 +25,10 @@ for line in lines:
     if line:
         if line.startswith('//'):
             if 'test_count' in locals() and 'pass_count' in locals():
-                print(f"\n{pass_count}/{test_count} tests passed.")
-                if fails:
-                    print("Failed tests:")
-                    for entry, expected, result in fails:
-                        print(f"[X] {entry} -> {result} (Expected: {expected})")
+                print_test_summary(pass_count, test_count, fails)
             fails = []
             test_count = pass_count = 0
-            print(f"\n[Test] {line[2:].strip()}")
+            print_test_header(line[2:].strip())
             
         else:
             entry, expected = [value.strip() for value in line.split('->')]
@@ -28,6 +37,9 @@ for line in lines:
                 expected_error = expected[5:].strip()
                 if ':' in expected_error:
                     expected_error = expected_error.split(':')[1].strip()
+                    
+            if expected.startswith('~'):
+                expected = expected[1:].strip()
                 
             tokens, err = Lexer(entry).tokenize()
             if not err:
@@ -36,9 +48,12 @@ for line in lines:
                     result, err = Interpreter(ast).interpret()
             
             test_count += 1
-            if (err and expected_error in err.__repr__()) or (not err and str(result) == expected):
+            if (err and 'expected_error' in locals() and expected_error in err.__repr__()) \
+            or (not err and math.isclose(float(result.value), float(expected), rel_tol=1e-6)):
                 pass_count += 1
                 print('.', end='')
             else:
                 print('x', end='')
                 fails.append((entry, expected, result if not err else err))
+                
+print_test_summary(pass_count, test_count, fails)
